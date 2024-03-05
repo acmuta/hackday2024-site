a3ddcfb12a064e8a47bfd9d62664ae32c61fefe7
dinklang <sdinklang@gmail.com>
2024-03-05 15:19:20 -0600

    profile

--- src/lib/auth/options.ts
+++ src/lib/auth/options.ts
@@ -29,29 +29,29 @@ export const nextauthOptions: NextAuthOptions = {
 
   providers: [
     GithubProvider({
-      //   profile(profile) {
-      //     console.log('Profile GH: ', profile)
+      profile(profile) {
+        console.log('Profile GH: ', profile)
 
-      //     let userRole = 'user'
-      //     return {
-      //       ...profile,
-      //       role: userRole,
-      //     }
-      //   },
+        let userRole = 'user'
+        return {
+          ...profile,
+          role: userRole,
+        }
+      },
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
     GoogleProvider({
-      //   profile(profile) {
-      //     console.log('Profile Google: ', profile)
+      profile(profile) {
+        console.log('Profile Google: ', profile)
 
-      //     let userRole = 'user'
-      //     return {
-      //       ...profile,
-      //       id: profile.sub,
-      //       role: userRole,
-      //     }
-      //   },
+        let userRole = 'user'
+        return {
+          ...profile,
+          id: profile.sub,
+          role: userRole,
+        }
+      },
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
@@ -68,38 +68,5 @@ export const nextauthOptions: NextAuthOptions = {
     }),
   ],
 
-  callbacks: {
-    // async signIn({ account, profile }) {
-    //   if (account?.type === 'oauth' && profile) {
-    //     return await signInWithOauth({ account, profile })
-    //   }
-    //   return true
-    // },
-    // async jwt({ token, trigger, session }) {
-    //   if (trigger === 'update') {
-    //     token.name = session.name
-    //   } else {
-    //     if (token.email) {
-    //       const user = await getUserByEmail({ email: token.email })
-    //       token.name = user.name
-    //       token._id = user._id
-    //       token.role = user.role
-    //       token.provider = user.provider
-    //     }
-    //   }
-    //   return token
-    // },
-    // async session({ session, token }) {
-    //   return {
-    //     ...session,
-    //     user: {
-    //       ...session.user,
-    //       name: token.name,
-    //       _id: token._id,
-    //       role: token.role,
-    //       provider: token.provider,
-    //     },
-    //   }
-    // },
-  },
+  callbacks: {},
 }
