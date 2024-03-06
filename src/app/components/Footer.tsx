fc2dfb3a932d63934023fab30563050dbaecd26b
dinklang <sdinklang@gmail.com>
2024-03-06 15:06:03 -0600

    bg

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-darkslategray flex md:flex-row flex-col items-center md:justify-start justify-center box-border text-left text-sm md:h-40 h-60 text-colour-main-grey-400 font-regular-14 space-x-10">
+    <footer className="bg-darkslategray flex md:flex-row flex-col items-center md:justify-start justify-center box-border text-left text-sm md:h-40 h-60 text-colour-main-grey-400 font-regular-14 md:space-x-10 space-x-0">
       <div className="flex flex-col justify-center items-center p-5">
         <img
           className="w-[74px] h-[74px] relative object-cover"
@@ -9,10 +9,10 @@ export default function Footer() {
           src="/hackutalogo-1@2x.png"
         />
       </div>
-      <div className="flex flex-col w-full">
-        <div className="flex flex-row items-start justify-center max-w-full">
+      <div className="flex flex-col">
+        <div className="flex flex-row items-start justify-center">
           <div className="relative leading-[24px] font-medium">
-            ©2023 Yourcompany
+            ©2023 ACM @ UTA
           </div>
         </div>
         <div className=" flex flex-row flex-wrap items-end justify-center gap-[0px_37px] text-colour-main-blue-300">
